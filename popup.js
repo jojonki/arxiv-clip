db426fac35e6e699a00f6dda81de0c0632198cbe
jojonki <junki.ohmura@gmail.com>
2022-12-29 23:15:17 +0900

    apply Prettier formatter

--- popup.js
+++ popup.js
@@ -1,30 +1,29 @@
 "use strict";
 
-
-
 function copyToClipboard(text) {
-    const input = document.createElement('textarea');
-    input.style.position = 'fixed';
-    //input.style.opacity = 0;
-    input.value = text;
-    document.body.appendChild(input);
-    input.select();
-    document.execCommand('Copy');
-    document.body.removeChild(input);
-};
+  const input = document.createElement("textarea");
+  input.style.position = "fixed";
+  //input.style.opacity = 0;
+  input.value = text;
+  document.body.appendChild(input);
+  input.select();
+  document.execCommand("Copy");
+  document.body.removeChild(input);
+}
 
-document.addEventListener('DOMContentLoaded', () => {
-    let sendMessage = {
-        'message': 'retrievePaperInfo',
-    }
-    chrome.runtime.sendMessage(sendMessage, function (response) {
-        let info = response.data;
-        copyToClipboard(info);
-        document.getElementById("result").textContent = "Copied information from " + info;
+document.addEventListener("DOMContentLoaded", () => {
+  let sendMessage = {
+    message: "retrievePaperInfo",
+  };
+  chrome.runtime.sendMessage(sendMessage, function (response) {
+    let info = response.data;
+    copyToClipboard(info);
+    document.getElementById("result").textContent =
+      "Copied information from " + info;
 
-        // hide popup automatically
-        setTimeout(function () {
-            window.close();
-        }, 3000);
-    });
+    // hide popup automatically
+    setTimeout(function () {
+      window.close();
+    }, 3000);
+  });
 });
