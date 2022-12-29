367510afba17577eced1d41190870a389b88073c
jojonki <junki.ohmura@gmail.com>
2022-12-29 23:02:54 +0900

    support mv3

--- background.js
+++ background.js
@@ -1,10 +1,75 @@
 "use strict";
-function checkForValidUrl(tabId, changeInfo, tab) {
-  if (tab.url.indexOf("https://arxiv.org") > -1) {
-    chrome.pageAction.show(tabId);
-  } else {
-    chrome.pageAction.hide(tabId);
-  }
-};
 
-chrome.tabs.onUpdated.addListener(checkForValidUrl);
+// Wrap in an onInstalled callback in order to avoid unnecessary work
+// every time the background script is run
+chrome.runtime.onInstalled.addListener(() => {
+    console.log("disabled!!")
+    // Page actions are disabled by default and enabled on select tabs
+    chrome.action.disable();
+
+    // Clear all rules to ensure only our expected rules are set
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+        // Declare a rule to enable the action on example.com pages
+        let onlyShowOnArxiv = {
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: { hostSuffix: 'arxiv.org' },
+                })
+            ],
+            actions: [new chrome.declarativeContent.ShowAction()],
+        };
+
+        // Finally, apply our new array of rules
+        let rules = [onlyShowOnArxiv];
+        chrome.declarativeContent.onPageChanged.addRules(rules);
+    });
+});
+
+function _retrievePaperInfo(arxiv_url) {
+    let abst = document.getElementById("abs");
+    let title = abst.querySelector("h1").textContent.split("Title:")[1].trim();
+    let authors = abst.querySelector('.authors').textContent.split('Authors:')[1].replace(/\n/g, '').trim();
+    let comment = abst.querySelector('.metatable tr:first-child td:nth-child(2)');
+    if (comment !== undefined) {
+        comment = comment.textContent.replace(/\n/g, '').trim();
+    }
+
+    let info = null;
+    if (comment != '') {
+        info = [title, authors, comment, arxiv_url].join('\n');
+    } else {
+        info = [title, authors, arxiv_url].join('\n');
+    }
+    return info
+}
+
+function retrievePaperInfo(sendResponse) {
+    chrome.tabs.query({
+        active: true,
+        lastFocusedWindow: true
+    }, function (tabs) {
+        chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            func: _retrievePaperInfo,
+            args: [tabs[0].url]
+        },
+            (results) => {
+                if (results.length > 0) {
+                    console.log("sendResponse: " + results[0].result);
+                    sendResponse({ data: results[0].result });
+                }
+            })
+    });
+    return true;
+}
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    switch (request.message) {
+        case 'retrievePaperInfo':
+            return retrievePaperInfo(sendResponse);
+
+        default:
+            sendResponse({ data: 'Invalid arguments' });
+            break;
+    }
+});
--- popup.js
+++ popup.js
@@ -1,56 +1,6 @@
 "use strict";
 
-let ARXIV_URL = 'https://arxiv.org/*';
 
-function retrieveInfoFromArxiv(arxiv_url, tab) {
-    $.ajax({
-        type: "GET",
-        url: arxiv_url,
-        success: function (data) {
-            let $dom = $($.parseHTML(data));
-            let title = $dom.find('h1.title').text().split('Title:')[1];
-            let authors = $dom.find('div.authors').text().split('Authors:')[1];
-            authors = authors.replace(/\n/g, '');
-            let comment = $dom.find('div.metatable').find('.comments').text();
-            let info = null;
-            if (comment != '') {
-                info = [title, authors, comment, arxiv_url].join('\n');
-            } else {
-                info = [title, authors, arxiv_url].join('\n');
-            }
-            copyToClipboard(info);
-            $('#result').text('copied!');
-
-            // hide popup automatically
-            setTimeout(function () {
-                window.close();
-            }, 3000);
-        }
-    });
-}
-
-function getCurrentTabUrl(callback) {
-    var queryInfo = {
-        url: ARXIV_URL,
-        active: true,
-        currentWindow: true
-    };
-
-    chrome.tabs.query(queryInfo, (tabs) => {
-        if (tabs.length > 0) {
-            var tab = tabs[0];
-            var url = tab.url;
-            console.assert(typeof url == 'string', 'tab.url should be a string');
-            callback(url, tab);
-        } else {
-            $('#result').text('not arXiv!');
-        }
-    });
-}
-
-function modifyDOM() {
-    return document.body.innerHTML;
-}
 
 function copyToClipboard(text) {
     const input = document.createElement('textarea');
@@ -64,21 +14,17 @@ function copyToClipboard(text) {
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-    getCurrentTabUrl((url, tab) => {
-        if (url.startsWith("https://arxiv.org/pdf/")) {
-            // from pdf page to abs page
-            url = url.replace('\.pdf', '');
-            url = url.replace('pdf', 'abs');
-            retrieveInfoFromArxiv(url, tab);
-        } else if (url.startsWith('https://arxiv.org/abs/')) {
-            retrieveInfoFromArxiv(url, tab);
-        } else {
-            $('#result').text('Unknown arXiv page style');
+    let sendMessage = {
+        'message': 'retrievePaperInfo',
+    }
+    chrome.runtime.sendMessage(sendMessage, function (response) {
+        let info = response.data;
+        copyToClipboard(info);
+        $('#result').text('copied!');
 
-            // hide popup automatically
-            setTimeout(function () {
-                window.close();
-            }, 3000);
-        }
+        // hide popup automatically
+        setTimeout(function () {
+            window.close();
+        }, 3000);
     });
 });
